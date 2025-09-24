088c7de5bcfa25664abcb7ca154a1b12b446887e
taynasuperti <taynasuperti@gmail.com>
2025-09-23 22:33:32 -0300

    Alterando o layout da página inicial - detalhes do crud

--- Pokedex/Models/Pokemon.cs
+++ Pokedex/Models/Pokemon.cs
@@ -7,12 +7,14 @@ namespace Pokedex.Models;
 public class Pokemon
 {
     [Key]
+    [Display(Name = "Número")]
     [DatabaseGenerated(DatabaseGeneratedOption.None)]
     public uint Numero { get; set; }
 
     [Required(ErrorMessage ="Por favor, informe a região")]
     public uint RegiaoId { get; set; }
     [ForeignKey("RegiaoId")]
+    [Display(Name = "Região")]
     public Regiao Regiao { get; set; }
 
     [Required(ErrorMessage ="Por favor, informe o genero")]
@@ -27,6 +29,7 @@ public class Pokemon
 
     [StringLength(1000)]
     public string Descricao { get; set; }
+    [Display(Name = "Descrição")]
 
     [Column(TypeName = "double(5,2)")]
     [Required(ErrorMessage ="Por favor, informe a altura")]
